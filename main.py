5be3fd572a14b8c568f8fbd0e110e1c3ee9155c9
orneryhippo <jesse.cassill@gmail.com>
2024-01-21 12:59:35 -0500

    Login, Register without overwrite working

--- main.py
+++ main.py
@@ -45,9 +45,15 @@ def index():
 
 @app.route('/register', methods=['POST','GET'])
 def register():
+    MSG = request.args.get('message',None) if request.method == 'GET' else request.form.get('message',None)
+
     if request.method == 'GET':
-        return render_template('register.html')
-    
+        ic(MSG)
+        if MSG is not None:
+            return render_template('register.html',message=MSG)
+        else:
+            return render_template('register.html', message=MSG)
+        
     user_id = request.form['email']
     plain_password = request.form['password']
 
@@ -62,42 +68,47 @@ def register():
     response  = httpx.post(store_user_url, headers=HEADERS, json=BODY)
     data = response.json()
     ic(data)
-    if response.status_code == 200 and data is not None:
+    if response.status_code == 200 and data.get('user_created',None) is not None:
         # return 'User registered successfully', response.status_code
-        return redirect(url_for('login',message="User Registered Successfully. Please Log In."))
+        return redirect(url_for('login', message="User Registered Successfully. Please Log In."))
     else:
-        return redirect(url_for('register',message="User Already Exists"))
+        return redirect(url_for('register', message="User Already Exists"))
 
 @app.route('/login', methods=['POST','GET'])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    
-    login_id = request.form['email']
-    plain_password = request.form['password']
-    # Retrieve the hashed password from your Xano database based on user_id
-    # https://x8ki-letl-twmt.n7.xano.io/api:KPiD297b/simple_login/{simple_login_id}
-    get_user_url = f"https://x8ki-letl-twmt.n7.xano.io/api:KPiD297b/simple_login_by_id?login_id={login_id}"
-    HEADERS={}
-    BODY={}
-    response = httpx.get(get_user_url,headers=HEADERS)
-    ic(response.status_code)
-    stored_hashed_password = ""
-    if (response.status_code == 200):
-        response_records = response.json()
-        if len(response_records) == 1:
-            user_record = response_records[0]
-            stored_hashed_password = user_record.get('password',"")
-    # Let's assume you get it in a variable `stored_hashed_password`
-    # ...
+    try:
+        if request.method == 'GET':
+            return render_template('login.html')
+        
+        login_id = request.form['email']
+        plain_password = request.form['password']
+        # Retrieve the hashed password from your Xano database based on user_id
+        # https://x8ki-letl-twmt.n7.xano.io/api:KPiD297b/simple_login/{simple_login_id}
+        get_user_url = f"https://x8ki-letl-twmt.n7.xano.io/api:KPiD297b/simple_login_by_id?login_id={login_id}"
+        HEADERS={}
+        BODY={}
+        response = httpx.get(get_user_url,headers=HEADERS)
+        ic(response.status_code)
+        stored_hashed_password = ""
+        if (response.status_code == 200):
+            response_records = response.json()
+            if len(response_records) == 1:
+                user_record = response_records[0]
+                stored_hashed_password = user_record.get('password',"")
+        # Let's assume you get it in a variable `stored_hashed_password`
+        # ...
 
-    # Verify the password
-    if bcrypt.check_password_hash(stored_hashed_password, plain_password):
-        ic('Login successful')
-        return render_template('protected.html',user=user_record.get('user_id',""))
-    else:
-        ic('Invalid user ID or password')
-        return render_template('login.html',message="Invalid Login")
+        # Verify the password
+        if bcrypt.check_password_hash(stored_hashed_password, plain_password):
+            ic('Login successful')
+            return render_template('protected.html',user=user_record.get('user_id',""))
+        else:
+            ic('Invalid user ID or password')
+            return render_template('login.html', message="Invalid Login")
+    except Exception as e:
+        ic(e)
+    # finally:
+        # return render_template('login.html', message=None)
 
 # @app.route('/login', methods=['GET', 'POST'])
 # def login():
